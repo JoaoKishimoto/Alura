dfd83e45006ea019ddac895c57fab5d395aaaa53
João Kishimoto <joaokishimoto@ufba.br>
2024-11-14 21:58:18 -0300

    change: Finished Carrinho de compras

---
+++ praticando-logica-desafios/carrinho-compras/js/app.js
@@ -0,0 +1,32 @@
+let valorCarrinho = 0;
+
+function adicionar() {
+    let produto = document.getElementById('produto');
+    let quantidade = document.getElementById('quantidade').value;
+    entradas = produto.value.split(" - R$");
+    let nome = entradas[0];
+    let preco = parseInt(entradas[1]);
+    let valorTotal = document.getElementById('valor-total');
+    let valor = preco * quantidade;
+    let carrinho = document.getElementById('lista-produtos');
+
+    carrinho.innerHTML += `<section class="carrinho__produtos__produto">
+        <span class="texto-azul">${quantidade}x</span> ${nome} <span class="texto-azul">R$${valor}</span>
+    </section>`
+
+    valorTotal.innerHTML = `R$${valorCarrinho += valor}`;
+
+    quantidade.value = 1;
+}
+
+function limpar() {
+
+}
+
+
+
+
+
+{/* <section class="carrinho__produtos__produto">
+    <span class="texto-azul">1x</span> Celular <span class="texto-azul">R$1400</span>
+</section> */}
